ac9bbd5c57fecec652fc602b3e188e2f343c6514
Uzikami <55492488+Uzikami@users.noreply.github.com>
2019-10-15 19:16:00 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var button = document.getElementById("inlogknop");
 var element = document.querySelector("form");
 
+var filterbutton = document.getElementById("filterknop");
+var filterelement = document.querySelector("div");
+
 button.addEventListener("click", function() {
     element.classList.toggle("laatzien");
+});
+
+filterbutton.addEventListener("click", function() {
+    filterelement.classList.toggle("filterlaatzien");
 });
 
